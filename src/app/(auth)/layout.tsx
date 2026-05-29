b6550e1c06911433d9fee97c539eae1fd5115263
Javier <javieralzuu@gmail.com>
2026-05-29 02:54:21 -0300

    fixtures and matches adding , flags added. predictions feature 50% done!

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,6 @@
+// app/(protected)/layout.tsx
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { getAuthenticatedUser } from "@/shared/utils/getAuthenticatedUser";
 import { Nav } from "@/shared/components/layout/Nav";
@@ -7,7 +10,7 @@ import { colors } from "@/shared/constants/designSystem";
 type ProtectedLayoutProps = {
   children: React.ReactNode;
 };
-// ProtectedLayout.tsx
+
 export default async function ProtectedLayout({
   children,
 }: ProtectedLayoutProps) {
@@ -19,8 +22,7 @@ export default async function ProtectedLayout({
       className="min-h-screen"
       style={{ backgroundColor: colors.background }}
     >
-      <Nav userName={user.name} />{" "}
-      {/* ← pasás el nombre, no llama auth de nuevo */}
+      <Nav userName={user.name} />
       <main className="pb-24" style={{ minHeight: "calc(100vh - 80px)" }}>
         {children}
       </main>
